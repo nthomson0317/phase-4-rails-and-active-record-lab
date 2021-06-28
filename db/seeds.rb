2ff8cb104d8b77e49a2a6e60f495c39e15490e0c
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-28 05:24:11 -0600

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: 'Nicholas', last_name: 'Thomson', grade: 26)
+Student.create(first_name: 'Annie', last_name: 'Bishai', grade: 12)
